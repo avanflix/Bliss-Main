69afac6ba72b2348abe0e8bed0c834ba962edaed
Likhil <likhilg0808@gmail.com>
2026-07-04 09:16:26 +0530

    gtags updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import FloatingActionButtons from "@/components/FloatingActionButtons";
 
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["400","500", "600", "700"],
+  weight: ["400", "500", "600", "700"],
 });
-
 
 export const metadata: Metadata = {
   title: "Bliss Ventures",
   description: "Bliss Ventures",
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon.ico',
-    apple: '/favicon.ico',
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
     other: {
-      rel: 'icon',
-      url: '/favicon.ico',
+      rel: "icon",
+      url: "/favicon.ico",
     },
   },
 };
@@ -32,13 +32,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-       <meta name="google-site-verification" content="zScuppe2wZNvfSGo9Zat0cRlXFqLoMfnc6irdgBTEEw" />
+        <meta
+          name="google-site-verification"
+          content="zScuppe2wZNvfSGo9Zat0cRlXFqLoMfnc6irdgBTEEw"
+        />
       </head>
-      <body
-        className={`${poppins.variable} antialiased`}
-      >
+
+      <body className={`${poppins.variable} antialiased`}>
         {children}
         <FloatingActionButtons />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QELCYZH447"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QELCYZH447');
+          `}
+        </Script>
       </body>
     </html>
   );
